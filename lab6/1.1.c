0f2481d25420c652054261ad3c2c9f84cbb57e79
golovnyakpa <pgolovnyak@gmail.com>
2018-12-18 18:53:23 +0300

    1.1 Количество переданных переменных окружения

---
+++ lab6/1.1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+extern char **environ;
+int main(int argc, char *argv[])
+{
+        char **p;
+        int counter;
+                for (p = environ; *p != NULL; p++) /* перебор всех элементов массива */
+                        counter++;
+        printf("Number of environment variables is %d\n", counter);
+}
