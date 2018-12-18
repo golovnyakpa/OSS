d25b81ba1e159c17cb630ea3d59125313f86456e
golovnyakpa <pgolovnyak@gmail.com>
2018-12-18 22:31:13 +0300

    2.2 Ни дочерний, ни родительский процесс не завершаются моментально

---
+++ lab6/2.2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(void)
+{	
+	int pid = fork();
+    if (pid == 0) {
+		sleep(5);
+		printf("Это сообщение из дочернего процесса.\n");
+    } else if (pid > 0) {
+		sleep(10);
+		printf("Это сообщение из родительского процесса.\n");
+    }
+    return 0;
+}
