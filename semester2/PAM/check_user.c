785ea87b3825e98d074cda0674a1e1f7e2566cda
Pavel Golovnyak <pgolovnyak@gmail.com>
2019-05-20 12:45:49 +0300

    Update check_user.c

--- semester2/PAM/check_user.c
+++ semester2/PAM/check_user.c
@@ -15,12 +15,12 @@ int main(int argc, char *argv[])
     const char *user="nobody";
     if(argc == 2) 
     {
-		    user = argv[1];
+		user = argv[1];
     }
     if(argc > 2) 
     {
-		    fprintf(stderr, "Usage: check_user [username]\n");
-		    exit(1);
+		fprintf(stderr, "Usage: check_user [username]\n");
+		exit(1);
     }
     retval = pam_start("check", user, &conv, &pamh);
     if (retval == PAM_SUCCESS)
@@ -31,17 +31,17 @@ int main(int argc, char *argv[])
     printf("error code: %s\n", pam_strerror(pamh, retval));
     if (retval == PAM_SUCCESS) 
     {
-		    fprintf(stdout, "Authenticated\n");
+		fprintf(stdout, "Authenticated\n");
     } 
     else 
     {
-		    fprintf(stdout, "Not Authenticated\n");
+		fprintf(stdout, "Not Authenticated\n");
     }
     if (pam_end(pamh,retval) != PAM_SUCCESS) 
     {     /* close Linux-PAM */
-		    pamh = NULL;
-		    fprintf(stderr, "check_user: failed to release authenticator\n");
-		    exit(1);
+		pamh = NULL;
+		fprintf(stderr, "check_user: failed to release authenticator\n");
+		exit(1);
     }
     return ( retval == PAM_SUCCESS ? 0:1 );       /* indicate success */
 }
